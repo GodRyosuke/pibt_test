2e18f94537dc4cfaffcf00d16ab82e3b2b046230
GodRyosuke <kusuprograming@gmail.com>
2025-08-11 18:09:02 +0900

    add isAvailableSpace

--- src/actor/localization_map_actor.cpp
+++ src/actor/localization_map_actor.cpp
@@ -6,7 +6,9 @@
 
 namespace wander_csm_test {
 LocalizationMapActor::LocalizationMapActor(Game& game)
-    : Actor(game, "localization_map")
+    : Actor(game, "localization_map"),
+      m_localizationImgPath(std::string(ASSET_PATH) + "map_data/Magnes_202501-01F/localizations/localization.png"),
+      m_pathDetectionTh(250)
 {
     MeshComponent& meshComponent   = game.createMeshComponent(getId(), std::string(ASSET_PATH) + "plane/plane.obj", "localizationMapShader");
     const auto&    localizationTex = game.loadTexture(m_localizationImgPath);
@@ -15,7 +17,7 @@ LocalizationMapActor::LocalizationMapActor(Game& game)
     const double   fieldWidth      = imgWidth * 0.05;
     const double   fieldHeight     = imgHeight * 0.05;
 
-    const auto localizationYaml = YAML::LoadFile(std::string(ASSET_PATH) + "Magnes_202501-01F/localizations/localization.yaml");
+    const auto localizationYaml = YAML::LoadFile(std::string(ASSET_PATH) + "map_data/Magnes_202501-01F/localizations/localization.yaml");
     double     originX          = localizationYaml["origin"][0].as<double>();
     double     originY          = localizationYaml["origin"][1].as<double>();
     m_origin                    = wu::Vec2(originX, originY);
@@ -24,10 +26,35 @@ LocalizationMapActor::LocalizationMapActor(Game& game)
     setPosition(wu::Vec3(fieldWidth / 2.0, fieldHeight / 2.0, 0.0) + wu::Vec3(originX, originY, -0.001));
     setRotation(wu::PI / 2.0);
     setScale(wu::Vec3(fieldHeight, fieldWidth, 1.));
-    
-    meshComponent.setTexture(localizationImgPath);
-    addComponent(meshComponent);
 
-    // setScale(2.0 / 5.0);
+    meshComponent.setTexture(m_localizationImgPath);
+    addComponent(meshComponent);
+}
+
+bool LocalizationMapActor::isAvailableSpace(const wu::Vec2& pos) const
+{
+    // 左上原点 y軸下向きの座標系に変換
+    const auto&  localizationTex = m_game.loadTexture(m_localizationImgPath);
+    const double imgWidth        = localizationTex.getWidth();
+    const double imgHeight       = localizationTex.getHeight();
+    const double fieldWidth      = imgWidth * 0.05;
+    const double fieldHeight     = imgHeight * 0.05;
+
+    wu::Vec2 imgCenterCoord = pos - wu::Vec2(fieldWidth / 2.0, fieldHeight / 2.0) - m_origin;
+    // 左下が原点でy軸上にflipしてloadされているから、この読み方で行う
+    wu::Vec2 cvCoord = imgCenterCoord + wu::Vec2(fieldWidth / 2.0, fieldHeight / 2.0);
+    // wu::Vec2 cvCoord = imgCenterCoord - wu::Vec2(-fieldWidth / 2.0, fieldHeight / 2.0);
+    // cvCoord.y() *= -1;
+
+    if ((cvCoord.x() < 0) || (cvCoord.x() >= fieldWidth) || (cvCoord.y() < 0) || (cvCoord.y() >= fieldHeight)) {
+        return false;
+    }
+
+    const wu::Vec2 imgPixel = cvCoord * 20;
+    auto           pixel    = localizationTex.getPixel(imgPixel.x(), imgPixel.y());
+    if (pixel < m_pathDetectionTh) {
+        return false;
+    }
+    return true;
 }
 }  // namespace wander_csm_test
--- src/actor/localization_map_actor.hpp
+++ src/actor/localization_map_actor.hpp
@@ -4,9 +4,16 @@
 #include "texture.hpp"
 
 namespace wander_csm_test {
+class Texture;
 class LocalizationMapActor : public Actor
 {
 public:
     LocalizationMapActor(Game& game);
+    bool isAvailableSpace(const wu::Vec2& pos) const;
+
+private:
+    std::string   m_localizationImgPath;
+    unsigned char m_pathDetectionTh;
+    wu::Vec2      m_origin;
 };
 }  // namespace wander_csm_test
