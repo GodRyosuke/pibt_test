a90caa5154b961ec61bf4aa64ec95a03252a6b79
GodRyosuke <kusuprograming@gmail.com>
2025-08-11 17:46:03 +0900

    add loading  localization.yml process

--- src/actor/localization_map_actor.cpp
+++ src/actor/localization_map_actor.cpp
@@ -2,18 +2,27 @@
 #include "component/mesh_component.hpp"
 #include "definitions.hpp"
 #include "game.hpp"
+#include "yaml-cpp/yaml.h"
 
 namespace wander_csm_test {
 LocalizationMapActor::LocalizationMapActor(Game& game)
     : Actor(game, "localization_map")
 {
-    MeshComponent& meshComponent = game.createMeshComponent(getId(), std::string(ASSET_PATH) + "plane/plane.obj", "localizationMapShader");
-    const std::string localizationImgPath =std::string(ASSET_PATH) + "localization.png";
-    const auto& localizationTex = game.loadTexture(localizationImgPath);
-    const double imgWidth = localizationTex.getWidth();
-    const double imgHeight = localizationTex.getHeight();
-    const double fieldWidth = imgWidth * 0.05;
-    const double fieldHeight = imgHeight * 0.05;
+    MeshComponent& meshComponent   = game.createMeshComponent(getId(), std::string(ASSET_PATH) + "plane/plane.obj", "localizationMapShader");
+    const auto&    localizationTex = game.loadTexture(m_localizationImgPath);
+    const double   imgWidth        = localizationTex.getWidth();
+    const double   imgHeight       = localizationTex.getHeight();
+    const double   fieldWidth      = imgWidth * 0.05;
+    const double   fieldHeight     = imgHeight * 0.05;
+
+    const auto localizationYaml = YAML::LoadFile(std::string(ASSET_PATH) + "Magnes_202501-01F/localizations/localization.yaml");
+    double     originX          = localizationYaml["origin"][0].as<double>();
+    double     originY          = localizationYaml["origin"][1].as<double>();
+    m_origin                    = wu::Vec2(originX, originY);
+
+    // setPosition(wu::Vec3(fieldWidth / 2.0, fieldHeight / 2.0, 0.0));
+    setPosition(wu::Vec3(fieldWidth / 2.0, fieldHeight / 2.0, 0.0) + wu::Vec3(originX, originY, -0.001));
+    setRotation(wu::PI / 2.0);
     setScale(wu::Vec3(fieldHeight, fieldWidth, 1.));
     
     meshComponent.setTexture(localizationImgPath);
